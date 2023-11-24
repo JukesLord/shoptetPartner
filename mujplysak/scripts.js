a3c79db93dff47147bcf0917a4dbf453954e8f14
JukesLord <flphosek@seznam.cz>
2023-11-24 01:37:53 +0100

    xc

--- mujplysak/scripts.js
+++ mujplysak/scripts.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 	$(".contact-box").append($(".footer-platby-online"));
+	$(".footer-platby-online").show();
+
+	if (document.body.classList.contains("in-index")) {
+		$(".kde-pomahame").insertAfter($(".dlazdice-wrapper"));
+	}
 });
