d6a7c6c64ed6a731895422f89bc3585f6cfda67a
JukesLord <flphosek@seznam.cz>
2024-09-27 19:46:29 +0200

    c

--- cukrarna/script.js
+++ cukrarna/script.js
@@ -11,8 +11,8 @@ if (document.body.classList.contains("type-category")) {
 		$(".category-top .category-perex").append($(".category-title"));
 	}
 }
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
 	let spolufinancovanoEU =
 		'<div class="financovan-eu"><div><img src="https://cdn.myshoptet.com/usr/www.verpet.cz/user/documents/upload/Financováno EU 1.png" caption="false" width="303" height="91" alt="Financováno EU"><img src="https://cdn.myshoptet.com/usr/www.verpet.cz/user/documents/upload/Financováno EU 2.png" caption="false" width="178" height="73" alt="Financováno EU"><img src="https://cdn.myshoptet.com/usr/www.verpet.cz/user/documents/upload/Financováno EU 3.png" caption="false" width="224" height="69" alt="Financováno EU" style="margin-left: 10px;"><p><span>Projekt 0461000736 byl financován Evropskou Unií.</span></p></div></div>';
 	$(spolufinancovanoEU).insertAfter(".custom-footer__articles.col-sm-4");
-});
+});*/
