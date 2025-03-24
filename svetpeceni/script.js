c315c18534de0cc8aa0a9de6464f958c4deb148e
JukesLord <flphosek@seznam.cz>
2025-03-24 16:46:16 +0100

    Update script.js

--- svetpeceni/script.js
+++ svetpeceni/script.js
@@ -1,10 +1,11 @@
-if ($("body").hasClass("in-krok-1") && $("body").hasClass("admin-logged")) {
+if ($("body").hasClass("in-krok-1")) {
 	addContactHours();
 
 	function addContactHours() {
 		let contactHours = `<div class="contact-hours">
-            <h4>Potřebujete poradit?</h4>
-            <p>Po-Pá: 8:00-16:00</p>
+            <h4>Potřebujete poradit? Volejte v časech:</h4>
+            <p>Po–Čt: 8:00–17:00</p>
+			<p>Pá: 8:00–15:00</p>
             </div>`;
 
 		$(contactHours).insertBefore($(".contact-box span.tel"));
