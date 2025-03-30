ec4fb7f90b83c253b60688cc3b710da9a97d52fd
JukesLord <flphosek@seznam.cz>
2025-03-30 19:58:28 +0200

    Update script.js

--- matexpon/script.js
+++ matexpon/script.js
@@ -17,14 +17,26 @@ if ($("body").hasClass("in-index")) {
 		indexFunctions();
 	});
 
-	if (window.matchMedia("(min-width: 1024px)").matches) {
-		// Adjust the breakpoint as needed
+	const userAgent = navigator.userAgent;
+
+	if (/Mobi|Android/i.test(userAgent)) {
+	} else if (/Tablet|iPad/i.test(userAgent)) {
+	} else {
 		$(document).on("resizeEnd", function () {
 			setTimeout(() => {
 				indexFunctions();
 			}, 1);
 		});
 	}
+
+	/* 	if (window.matchMedia("(min-width: 1024px)").matches) {
+		// Adjust the breakpoint as needed
+		$(document).on("resizeEnd", function () {
+			setTimeout(() => {
+				indexFunctions();
+			}, 1);
+		});
+	} */
 
 	function removeNavigation(wrapper) {
 		wrapper.find(".product-slider-pagination").addClass("custom-display-none");
